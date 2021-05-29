d5925f9944ebf95367fe73b80f08a950cb737e88
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-05-29 11:12:41 +0200

    Serialización terminada

--- Practica_2.2/ejercicios123.cc
+++ Practica_2.2/ejercicios123.cc
@@ -15,7 +15,7 @@ class Jugador: public Serializable
 public:
     Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
     {
-        strncpy(name, _n, 80);
+        strncpy(name, _n, MAX_NAME);
     };
 
     virtual ~Jugador(){};
@@ -23,12 +23,12 @@ public:
     void to_bin()
     {
         //
-        int size = (80*sizeof(char)) + (2* sizeof(x));
+        int size = (MAX_NAME*sizeof(char)) + (2* sizeof(x));
         alloc_data(size);
         char* tmp= _data;
 
-        memcpy(tmp,name, 80*sizeof(char));
-        tmp = tmp+80*sizeof(char);
+        memcpy(tmp,name, MAX_NAME*sizeof(char));
+        tmp = tmp+(MAX_NAME*sizeof(char));
         memcpy(tmp,&x,sizeof(x));
         tmp = tmp+sizeof(x);
         memcpy(tmp,&y,sizeof(y));
@@ -38,11 +38,12 @@ public:
     int from_bin(char * data)
     {
         char* tmp = data;
-        memcpy(name,tmp, 80*sizeof(char));
-        tmp = tmp+80*sizeof(char);
+        memcpy(name,tmp, MAX_NAME*sizeof(char));
+        tmp = tmp+(MAX_NAME*sizeof(char));
         memcpy(&x,tmp,sizeof(x));
         tmp = tmp+sizeof(x);
         memcpy(&y,tmp,sizeof(y));
+
         return 0;
     }
 
@@ -56,12 +57,11 @@ public:
     char* getName(){
         return name;
     }
-    void prueba(){
-        strncpy(name, "Hola que tal estas\0", 80);
-    }
 
 public:
-    char name[80];
+    static const size_t MAX_NAME = 20;
+
+    char name[MAX_NAME];
 
     int16_t x;
     int16_t y;
@@ -70,20 +70,18 @@ public:
 int main(int argc, char **argv)
 {
     Jugador one_r("", 0, 0);
-    Jugador one_w("HolaBuenas", 123, 987);
+    Jugador one_w("Jugador1", 123, 987);
 
     int id =0;
     // 1. Serializar el objeto one_w
-    id = open("./dataJugador",O_CREAT |  O_TRUNC | O_RDWR , 6666);
+    id = open("./dataJugador",O_CREAT |  O_TRUNC | O_RDWR , 00666);
     one_w.to_bin();
 
     // 2. Escribir la serialización en un fichero
     write(id,one_w.data(),one_w.size());
     close(id);   
 
-    one_w.prueba();
-    one_w.from_bin(one_w.data());
-    std::cout<<"Nombre= "<<one_w.getName()<<" X= "<<one_w.getX()<<" Y= "<<one_w.getY()<< std::endl;
+    std::cout<<"Soy el lector antes de leer\n Nombre= "<<one_r.getName()<<" X= "<<one_r.getX()<<" Y= "<<one_r.getY()<< std::endl;
 
     // 3. Leer el fichero
     char* lector = (char*)malloc(one_w.size());
@@ -94,7 +92,7 @@ int main(int argc, char **argv)
     one_r.from_bin(lector);
 
     // 5. Mostrar el contenido de one_r
-    std::cout<<"Nombre= "<<one_r.getName()<<" X= "<<one_r.getX()<<" Y= "<<one_r.getY()<< std::endl;
+    std::cout<<"Soy el lector despues de leer\n Nombre= "<<one_r.getName()<<" X= "<<one_r.getX()<<" Y= "<<one_r.getY()<< std::endl;
     free(lector);
 
     return 0;
